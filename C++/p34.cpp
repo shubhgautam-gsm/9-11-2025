e960e86c3b8434321cba55925e882265bd12f7cd
shubhgautam-gsm <143082586+shubhgautam-gsm@users.noreply.github.com>
2025-02-06 11:03:40 +0530

    6-2-25

---
+++ C++/p34.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Declare an array of strings
+  // not need strict to give size to like  string cars[4]; i.e dyanmic
+  string cars[] = {"Volvo", "BMW", "Ford", "Mazda"};
+  cout << cars[3];
+  return 0;
+}
