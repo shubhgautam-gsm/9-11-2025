0a216cf25de44e310836c182a8a6519b9900b27f
shubhgautam-gsm <143082586+shubhgautam-gsm@users.noreply.github.com>
2025-08-23 10:40:53 +0530

    23

--- python/unit 9 modules/p1.py
+++ python/unit 9 modules/p1.py
@@ -1,3 +1,7 @@
-import file
+# import file 
+from .file import displayMsg
+
 name = input("Enter the name?")
-file.displayMsg(name)
+
+# file.displayMsg(name)
+displayMsg(name)
--- python/unit 9 modules/pro2.py
+++ python/unit 9 modules/pro2.py
@@ -1,6 +1,10 @@
-flicart={'mobile':['iphone','nokia','samsung'],
-         'laptop':['asus',{'hp':['red','green','blue']}],
+flicart={
+         'mobile':['iphone','nokia','samsung'],
+         
+         'laptop':[{'asus':['golden','silver']},{'hp':['red','green','blue']}],
+         
          'tv':['lg','samsung']
+         
          }
 
-print(flicart['laptop'][1]['hp'][1])
+print(flicart['laptop'][0]['asus'][1])
